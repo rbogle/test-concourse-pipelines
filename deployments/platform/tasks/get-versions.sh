d5e6ca2d13c630d697ae92ffbc461d6dc6dc1d6a
Rian Bogle <rian.bogle@gsa.gov>
2022-08-26 11:55:35 -0700

    renames and moves

--- deployments/platform/tasks/get-versions.sh
+++ deployments/platform/tasks/get-versions.sh
@@ -2,7 +2,7 @@
 
 # login to fly and get token
 fly -t ${CONCOURSE_TARGET} login -c ${CONCOURSE_URL} -u ${CONCOURSE_USER} -p ${CONCOURSE_PASSWD}
-fly -t ${CONCOURSE_TARGET} sync
+fly -t ${CONCOURSE_TARGET} sync 
 BEARER_TOKEN=$(cat ~/.flyrc | grep value | awk '{ print $2 }')
 echo "${BEARER_TOKEN}" >  versions/bearer-token
 
@@ -15,5 +15,8 @@ BUILD_TEAM_NAME=$(cat metadata/build-team-name)
 # Output build info
 VERSIONS=$(fly -t ${CONCOURSE_TARGET} curl /api/v1/builds/${BUILD_ID}/resources  -- -s | jq '.inputs | map( { (.name): .version } ) | add')
 echo ${VERSIONS} | yq -P > versions/resource_versions.yml 
+
+echo
+echo "Resource Versions used in build:"
 echo ${VERSIONS} | jq 
 
