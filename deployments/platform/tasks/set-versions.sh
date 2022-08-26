845abfa017d03694761a58477871d0434b3f002e
Rian Bogle <rian.bogle@gsa.gov>
2022-08-26 11:31:19 -0700

    remove output

--- deployments/platform/tasks/set-versions.sh
+++ deployments/platform/tasks/set-versions.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-exec 3>&1 # make stdout available as fd 3 for the result
-exec 1>&2 # redirect all output to stderr for logging
-
 # login to fly and get token
 fly -t ${CONCOURSE_TARGET} login -c ${CONCOURSE_URL} -u ${CONCOURSE_USER} -p ${CONCOURSE_PASSWD}
 fly -t ${CONCOURSE_TARGET} sync
@@ -16,6 +13,7 @@ BUILD_PIPELINE_NAME=$(cat metadata/build-pipeline-name)
 BUILD_TEAM_NAME=$(cat metadata/build-team-name)
 
 # Output build info
-versions=$(fly -t ${CONCOURSE_TARGET} curl /api/v1/builds/${BUILD_ID}/resources  -- -s | jq '.inputs | map( { (.name): .version } ) | add')
-echo ${versions} | yq -P > versions/resource_versions.yml 
-echo ${versions} >&3
+VERSIONS=$(fly -t ${CONCOURSE_TARGET} curl /api/v1/builds/${BUILD_ID}/resources  -- -s | jq '.inputs | map( { (.name): .version } ) | add')
+echo ${VERSIONS} | yq -P > versions/resource_versions.yml 
+
+
