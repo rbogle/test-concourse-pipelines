e5025801afaaa75c9367e682aeb223a321d10d92
Rian Bogle <rian.bogle@gsa.gov>
2022-09-01 10:11:22 -0700

    commit outputs

--- deployments/platform/tasks/set-versions.sh
+++ deployments/platform/tasks/set-versions.sh
@@ -4,7 +4,7 @@
 fly -t ${CONCOURSE_TARGET} login -c ${CONCOURSE_URL} -u ${CONCOURSE_USER} -p ${CONCOURSE_PASSWD}
 fly -t ${CONCOURSE_TARGET} sync 
 BEARER_TOKEN=$(cat ~/.flyrc | grep value | awk '{ print $2 }')
-echo "${BEARER_TOKEN}" >  versions/bearer-token
+echo "${BEARER_TOKEN}" >  metadata/bearer-token
 
 BUILD_ID=$(cat metadata/build-id)
 BUILD_NAME=$(cat metadata/build-name)
@@ -15,6 +15,10 @@ BUILD_TEAM_NAME=$(cat metadata/build-team-name)
 # Output build info
 VERSIONS=$(fly -t ${CONCOURSE_TARGET} curl /api/v1/builds/${BUILD_ID}/resources  -- -s | jq '.inputs | map( { (.name): .version } ) | add')
 echo ${VERSIONS} | yq -P > versions/resource_versions.yml 
+
+if [ -n "${OUTPUT_PATH}" ] then
+    echo ${VERSIONS} | yq -P > ${OUTPUT_FILE}
+fi
 
 echo
 echo "Resource Versions used in build:"
