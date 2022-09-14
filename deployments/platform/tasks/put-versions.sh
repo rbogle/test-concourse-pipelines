90b6664586974e9cb596e24b6bc1bdd2ea331fd4
Rian Bogle <rian.bogle@gsa.gov>
2022-09-14 15:37:40 -0400

    put versions

--- deployments/platform/tasks/put-versions.sh
+++ deployments/platform/tasks/put-versions.sh
@@ -4,9 +4,12 @@
 cp ./versions/resource_versions.yml ./pipelines_source/${OUTPUT_FILE}
 
 cd pipelines_source
+
+git config user.email "cg-ci-bot@cloud.gov"
+git config user.name "cg-ci-bot"
+
 if [ -n "${SOURCE_BRANCH}"]; then
     git switch -c ${SOURCE_BRANCH}
 fi
 
-git commit -a -m "commiting build's resource versions"
-
+git commit -a -m "commiting build's resource version data"
